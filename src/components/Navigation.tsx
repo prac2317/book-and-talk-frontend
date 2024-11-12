e7cf9e4cc4e4ea6a38ff61fcf37a1da2b9863c87
prac2317 <lowdy92@naver.com>
2024-11-13 02:11:34 +0900

    design: 하단 nav바 이미지 넣기

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,22 +2,57 @@ import { NavLink } from 'react-router-dom';
 import styles from './navigation.module.css';
 
 const navigations = [
-  { to: '/', label: 'Home' },
-  { to: '/map', label: 'Map' },
-  { to: '/like', label: 'Like' },
-  { to: '/chat', label: 'Chat' },
-  { to: '/profile', label: 'Profile' },
+  {
+    to: '/',
+    label: '홈',
+    activeIcon: '/icon/Home-on.png',
+    inactiveIcon: '/icon/Home-off.png',
+  },
+  {
+    to: '/like',
+    label: '즐겨찾기',
+    activeIcon: '/icon/Star-on.png',
+    inactiveIcon: '/icon/Star-off.png',
+  },
+  {
+    to: '/chat',
+    label: '채팅',
+    activeIcon: '/icon/Chat-on.png',
+    inactiveIcon: '/icon/Chat-off.png',
+  },
+  {
+    to: '/profile',
+    label: '마이페이지',
+    activeIcon: '/icon/People-on.png',
+    inactiveIcon: '/icon/People-off.png',
+  },
 ];
 
 const Navigation = () => {
   return (
-    <div className={styles.nav}>
-      {navigations.map((nav) => (
-        <NavLink key={nav.to} to={nav.to} end>
-          {nav.label}
-        </NavLink>
-      ))}
-    </div>
+      <div className={styles.nav}>
+        {navigations.map((nav) => (
+            <NavLink
+                key={nav.to}
+                to={nav.to}
+                end
+                className={({ isActive }) =>
+                    isActive ? `${styles.navItem} ${styles.active}` : styles.navItem
+                }
+            >
+              {({ isActive }) => (
+                  <>
+                    <img
+                        src={isActive ? nav.activeIcon : nav.inactiveIcon}
+                        alt={nav.label}
+                        className={`${styles.navIcon} ${isActive ? 'active' : 'inactive'}`}
+                    />
+                    <span className={isActive ? styles.active : ''}>{nav.label}</span>
+                  </>
+              )}
+            </NavLink>
+        ))}
+      </div>
   );
 };
 
