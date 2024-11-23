e9f8b0aad38f056bf6883b2ff9252441b667156b
prac2317 <lowdy92@naver.com>
2024-11-24 05:20:26 +0900

    feat: 마이페이지 추가

---
+++ src/pages/profile/Logout.tsx
@@ -0,0 +1,36 @@
+import ky from 'ky';
+import { useNavigate } from 'react-router-dom';
+import styles from './profile.module.css';
+
+const Logout = () => {
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    try {
+      // 로그아웃을 위한 GET 요청
+      const response = await ky.get('http://localhost:8080/logout', {
+        credentials: 'include', // 쿠키 포함
+        throwHttpErrors: false, // 2xx가 아닌 응답에서도 에러를 던지지 않음
+      });
+
+      if (response.status === 200) {
+        // const data = await response.json();
+        console.log('로그아웃 성공:');
+        localStorage.removeItem('access_token');
+        navigate('/login');
+      } else {
+        console.error(`로그아웃 중 예상치 못한 응답 상태: ${response.status}`);
+      }
+    } catch (error) {
+      console.error('로그아웃 중 오류:', error);
+    }
+  };
+
+  return (
+    <button className={styles.logout123} onClick={logout}>
+      로그아웃
+    </button>
+  );
+};
+
+export default Logout;
